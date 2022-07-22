c3216b3a90388a858d8077880f41b5ed1f9e1942
Muhammad Jamshaid Ghaffar <muhammadjamshaid89@gmail.com>
2022-07-21 22:58:08 -0700

    created main panel  with css

---
+++ src/QuestionPanel.tsx
@@ -0,0 +1,12 @@
+
+export default function QuestionPanel(){
+    return <div className="question-panel">
+        <p className="question-no">Question: 1/10</p>
+        <p className="question">What is you name?</p>
+        <button className="option">Option 1</button>
+        <button className="option">Option 1</button>
+        <button className="option">Option 1</button>
+        <button className="option">Option 1</button>
+
+    </div>
+}
